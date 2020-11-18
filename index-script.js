b50a53f94ccdd858e07eee2f97f6717c9dad89f6
Joshua Minton <joshuamntn@gmail.com>
2020-11-18 14:01:20 +0000

    Added working check and delete buttons.

--- index-script.js
+++ index-script.js
@@ -1,10 +1,13 @@
 const todoList = document.querySelector('#todoList')
 
+// var todos = new Array();
+
 const btn = document.querySelector('.addWrapper');
 btn.addEventListener('click', () => {
     //https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template
     var template = document.querySelector('#todoTemp');
-    var clone = template.content.cloneNode(true);
+    var clone = template.content.firstElementChild.cloneNode(true);
     clone.querySelector('.taskText').innerHTML = document.getElementById('task').value;
+    clone.querySelector('.checkbox').onclick = function() { this.parentNode.classList.toggle("done"); };
     todoList.appendChild(clone);
 })
